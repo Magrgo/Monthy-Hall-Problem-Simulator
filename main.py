792a96cabbd2065f8e74e275487abbfb44eb6306
Magrgo <mgergo.it@gmail.com>
2024-03-27 13:46:08 +0100

    auto-simulation_added

--- main.py
+++ main.py
@@ -43,15 +43,14 @@ def credits():
     win.rowconfigure((0,1,2,3,4,5), weight=1)
     win.columnconfigure(0, weight=1)
     it_font = font.Font(slant="italic")
-    sum = Label(win, text="A program to simulate the Monty Hall problem", font=it_font)
+    sum = Label(win, text=",,A program to simulate the Monty Hall problem''", font=it_font)
     sum.grid(row=0, column=0)
     bold_font = font.Font(weight="bold")
     creator = Label(win, text="Made by Gergo Magyar", font=bold_font)
     creator.grid(row=1, column=0, sticky="n")
     gitb = Button(win, image=github_icon, borderwidth=0, command=lambda: webopen("github.com/Magrgo"))
     gitb.grid(row=2, column=0, sticky="n")
-    roman_font = font.Font(slant="roman")
-    picc = Label(win, text="Images from Vecteezy.com", font=roman_font)
+    picc = Label(win, text="Images from Vecteezy.com", font=bold_font)
     picc.grid(row=4, column=0)
     backb = Button(win, text="Back", font=("Calibri", 10), command=lambda:win.destroy())
     backb.grid(row=5, column=0)
@@ -89,7 +88,7 @@ class Door:
 
 #Using threading because of tkinter so that simulation runs separately from GUI
 def sthread():
-    t = threading.Thread(target=simulate)
+    t = threading.Thread(target=simdecide)
     t.start()
 
 
@@ -116,7 +115,7 @@ def readstat():
 
 
 
-#Edit statistics of winningW
+#Edit statistics of winning
 def addstat(win, bySwitch):
     f = open(f"{current_dir}\\stat.txt")
     all_withsw = int(f.readline())
@@ -151,6 +150,9 @@ def clearstat():
 #Reset everything after done with game
 def reset():
     simb.config(state=NORMAL)
+    autocb.config(state=NORMAL)
+    simb.config(text="Simulate")
+    simb.config(command=sthread)
     title.config(text="MH problem simulator")
     door1.close()
     door2.close()
@@ -165,7 +167,6 @@ def simulate():
         door.isActive = True
     dnums = [1,2,3]
     winner_door_num = random.randint(1,3)
-    print(winner_door_num)
     dnums.remove(winner_door_num)
     
     #Constantly checking if a door has been selected
@@ -259,6 +260,125 @@ def simulate():
     reset()
 
 
+#Stopping simulation(waits until the end and then stops it)
+def stopsim():
+    autosim.set(False)
+    autocb.config(state=DISABLED)
+    simb.config(state=DISABLED)
+
+
+#Custom sleep function for auto simulation, time divided by speed (SleepWithSpeed)
+def sleepws(time):
+        currspeed = simspeed.get()     
+        sleep(time/currspeed)
+
+
+#Automatic simulation
+def auto_simulate():
+    sc.config(state=NORMAL)
+    while autosim.get():
+        title.config(text="Choose a door!")
+        simb.config(text="Stop")
+        simb.config(command=stopsim)
+
+        dnums = [1,2,3]
+        winner_door_num = random.randint(1,3)
+        dnums.remove(winner_door_num)
+        sleepws(1.5)
+        
+        for door in [door1, door2, door3]:          #Activating doors for selection
+            door.isActive = True
+
+        selected_door_num = random.randint(1,3)
+        ([door1,door2,door3][selected_door_num-1]).mark()
+        ([door1,door2,door3][selected_door_num-1]).isMarked = False
+
+        for door in [door1, door2, door3]:      #Deactivating doors for dialog
+            door.isActive = False
+        sleepws(1.5)
+
+        title.config(text="Okay!")
+        sleepws(1.5)
+        title.config(text="Now I'll help you a little!")
+        sleepws(2)
+        try:
+            dnums.remove(selected_door_num)
+        except ValueError:                      #Delete marked/selected door from remaining. If it was already deleted(cause it's the winner), then pass 
+            pass
+        
+        #Select a random door from remaining and open it
+        odoor_num = random.choice(dnums)
+        if odoor_num == 1:
+            door1.open()
+        elif odoor_num == 2:
+            door2.open()
+        elif odoor_num == 3:
+            door3.open()
+        sleepws(1.5)
+
+        title.config(text="I opened a door for you!")
+        sleepws(2)
+        title.config(text="Do you want to change?")
+        sleepws(1.5)
+        title.config(text="Confirm your choice!")
+
+        i = 1
+        for door in [door1, door2, door3]:
+            if i != odoor_num:                             #Activating the doors except the one that's opened
+                door.isActive = True
+            i += 1
+
+        previous_door_num = selected_door_num
+        selected_door_num = random.randint(1,3)
+        while selected_door_num == odoor_num:
+            selected_door_num = random.randint(1,3)
+        ([door1,door2,door3][selected_door_num-1]).mark()
+        ([door1,door2,door3][selected_door_num-1]).isMarked = False
+
+        switched = False
+        if previous_door_num == selected_door_num:
+            title.config(text="You stayed!")
+        else:
+            ([door1,door2,door3][previous_door_num-1]).close()     #Deleting the mark from previous door
+            title.config(text="You switched!")
+            switched = True
+        sleepws(1.5)
+
+        title.config(text="Let's see the prize!")
+        sleepws(2)
+        ([door1,door2,door3][winner_door_num-1]).show_win()
+        sleepws(1.5)
+        phrases = {1:"first", 2:"second", 3:"third"}
+        title.config(text=f"Behind the {phrases[winner_door_num]} door!")
+        sleepws(1.5)
+
+        win = (selected_door_num == winner_door_num)
+        if win:
+            title.config(text="You win!")
+        else:                                          #Decide if we won, and save stats
+            title.config(text="You lose!")
+        addstat(win, switched)
+        sleepws(1.5)
+
+        all_sw, win_sw, all_nosw, win_nosw, perc_sw, perc_nosw = readstat()
+        perc.config(text=f"Win by switching: {win_sw}\{all_sw}: {perc_sw}%")
+        perc2.config(text=f"Win by staying: {win_nosw}\{all_nosw}: {perc_nosw}%")
+        reset()
+        sleepws(0.5)
+    else:
+        simspeed.set(1)
+        sc.config(state=DISABLED)
+
+
+#Function for deciding if the simulation is automatic or not
+def simdecide():
+    if autosim.get():
+        auto_simulate()
+    else:
+        simulate()
+
+
+#GUI Widgets:
 mFrame = Frame(root, background="white")
 mFrame.grid(row=0, column=0, sticky="nsew")
 mFrame.rowconfigure((0,1,2,3,4), weight=1)
@@ -275,9 +395,17 @@ door1 = Door(mFrame, 1)
 door2 = Door(mFrame, 2)
 door3 = Door(mFrame, 3)
 
+simspeed = IntVar()
+simspeed.set(1.0)
+sc = Scale(mFrame, variable=simspeed, from_=1, to=8)
+sc.grid(row=1, column=3, sticky="e")
+sc.config(state=DISABLED)
+
 simb = Button(mFrame, text="Simulate", font=('Calibri', 15), command=sthread)
 simb.grid(row=2, column=2)
-autocb = Checkbutton(mFrame, text="Auto-simulate")
+
+autosim = BooleanVar(root)
+autocb = Checkbutton(mFrame, text="Auto-simulate", variable=autosim)
 autocb.grid(row=2, column=2, sticky="s")
 
 all_sw, win_sw, all_nosw, win_nosw, perc_sw, perc_nosw = readstat()
